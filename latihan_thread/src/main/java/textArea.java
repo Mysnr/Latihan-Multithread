5421d2c8bcf4de58c96e7004363b20081e1d86fe
Ilma Nurlisnawati Hidayat <60433819+Mysnr@users.noreply.github.com>
2020-08-02 20:10:38 +0700

    Add files via upload

---
+++ latihan_thread/src/main/java/textArea.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Mays
+ */
+class textArea {
+
+    static void append(String string) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
+
+
